89b840e06e1b00a2f7a7e757d979aa1d8f0b0bb2
pratesi eclipse <pratesi.maurizio@gmail.com>
2013-10-16 13:44:30 +0200

    aggiunta una listview alla home

--- ProvaKendoUI/scripts/app.js
+++ ProvaKendoUI/scripts/app.js
@@ -1,1 +1,3 @@
 var app = new kendo.mobile.Application(document.body);
+
+window.drinks = [ "Caffè", "Coca cola", "Aranciata", "Birra", "Vino", "Acqua 1/2 l", "Acqua 1l", "Spuma", "Cedrata", "Vodka", "Martini", "Rum", "Whisky", "Succo di frutta", "Cappuccino", "Marocchino", "Caffè Shakerato", "Gin", "Altre Bevute Alcoliche", "Altre bevute Analcoliche" ];
